7a5a007fd5b51ba64ba3cd576ff493d1fca245d3
Lashkevich Ryan <lashkevichrain@gmail.com>
2023-02-25 17:32:56 +0700

    refactor(): server and client tests objects

--- src/Networking.Shared.ServerTests/Program.cs
+++ src/Networking.Shared.ServerTests/Program.cs
@@ -1,40 +1,4 @@
-﻿using System.Net.Sockets;
-using DataStreaming.Protocols.Factories;
-using DataStreaming.Services.RTT;
-using DataStreaming.Settings;
-using Microsoft.Extensions.Configuration;
+﻿using Networking.Shared.ServerTests;
 
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-var hostSettings = configuration.GetSection(HostSettings.SectionName).Get<HostSettings>();
-var protoFactory = (IRttMeteringProtocolFactory)RttMeteringProtocolFactory.Create();
-var server = new RttMeteringServer(hostSettings, protoFactory);
-server.ListeningStarted += (o, eventArgs) =>
-{
-    Console.WriteLine($"[{nameof(RttMeteringServer)}]: Listening at {eventArgs.EndPoint}");
-    return Task.CompletedTask;
-};
-
-try
-{
-    _ = await server.Start();
-}
-catch (SocketException e)
-{
-    //todo: log here
-    Console.WriteLine(e);
-    await server.DisposeAsync();
-    Environment.Exit(e.ErrorCode);
-}
-catch (Exception e)
-{
-    //todo: log here
-    Console.WriteLine($"Unexpected exception: {e}");
-    Environment.Exit(e.HResult);
-}
-finally
-{
-    await server.DisposeAsync();
-}
+var serverTests = new ServerTests();
+await serverTests.RttTest();
---
+++ src/Networking.Shared.ServerTests/ServerTests.cs
@@ -0,0 +1,48 @@
+using System.Net.Sockets;
+using DataStreaming.Protocols.Factories;
+using DataStreaming.Services.RTT;
+using DataStreaming.Settings;
+using Microsoft.Extensions.Configuration;
+
+namespace Networking.Shared.ServerTests;
+
+public class ServerTests
+{
+    public async Task RttTest()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var hostSettings = configuration.GetSection(HostSettings.SectionName).Get<HostSettings>();
+        var protoFactory = (IRttMeteringProtocolFactory)RttMeteringProtocolFactory.Create();
+        var server = new RttMeteringServer(hostSettings, protoFactory);
+        server.ListeningStarted += (o, eventArgs) =>
+        {
+            Console.WriteLine($"[{nameof(RttMeteringServer)}]: Listening at {eventArgs.EndPoint}");
+            return Task.CompletedTask;
+        };
+
+        try
+        {
+            _ = await server.Start();
+        }
+        catch (SocketException e)
+        {
+            //todo: log here
+            Console.WriteLine(e);
+            await server.DisposeAsync();
+            Environment.Exit(e.ErrorCode);
+        }
+        catch (Exception e)
+        {
+            //todo: log here
+            Console.WriteLine($"Unexpected exception: {e}");
+            Environment.Exit(e.HResult);
+        }
+        finally
+        {
+            await server.DisposeAsync();
+        }
+    }
+}
