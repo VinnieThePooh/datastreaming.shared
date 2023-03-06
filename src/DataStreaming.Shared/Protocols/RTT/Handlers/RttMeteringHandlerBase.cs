cf4d21135536d5afedcddecdb7f512250435a61b
Lashkevich Ryan <lashkevichrain@gmail.com>
2023-03-06 16:00:10 +0700

    refactor(): base handler

--- src/DataStreaming.Shared/Protocols/RTT/Handlers/RttMeteringHandlerBase.cs
+++ src/DataStreaming.Shared/Protocols/RTT/Handlers/RttMeteringHandlerBase.cs
@@ -1,9 +1,11 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using DataStreaming.Constants.RTT;
 using DataStreaming.Events;
 using DataStreaming.Events.RTT;
+using DataStreaming.Exceptions;
 using DataStreaming.Extensions;
 using DataStreaming.Models.RTT;
 using DataStreaming.Settings;
@@ -76,7 +78,29 @@ public abstract class RttMeteringHandlerBase : IRttMeteringHandler
         streamingInfo.ConstructMessage();
         return streamingInfo;
     }
+    
+    protected virtual async Task StartReceiving(Socket party, CancellationToken token)
+    {
+        var streamInfo = RttStreamingInfo.InitWithPacketSize(_settings.PacketSize);
 
+        while (!token.IsCancellationRequested)
+        {
+            streamInfo = await ReadStreamData(party, token, streamInfo);
+            if (streamInfo.IsDisconnectedPrematurely)
+                throw new DisconnectedPrematurelyException();
+
+            var message = streamInfo.Message;
+            if (_statsMap.TryRemove(message.SequenceNumber, out var stats))
+            {
+                stats.RttValue = Stopwatch.GetElapsedTime(stats.SendTimeTrace, message.Timetrace);
+                InvokeReceivedStatsEvent(this, new RttStatisticsEventArgs(stats.SequenceNumber, stats));
+            }
+        }
+
+        token.ThrowIfCancellationRequested();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected void InvokeReceivedStatsEvent(RttMeteringHandlerBase sender, RttStatisticsEventArgs eventArgs) =>
         RttReceived?.Invoke(sender, eventArgs);
 }
--- src/DataStreaming.Shared/Protocols/RTT/Handlers/SinglePacketHandler.cs
+++ src/DataStreaming.Shared/Protocols/RTT/Handlers/SinglePacketHandler.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
 using System.Net.Sockets;
-using DataStreaming.Events.RTT;
-using DataStreaming.Exceptions;
 using DataStreaming.Models.RTT;
 using DataStreaming.Settings;
 
@@ -26,26 +23,5 @@ public class SinglePacketHandler : RttMeteringHandlerBase
             _statsMap.TryAdd(counter, stats);
             IncrementCounter(memory.Span, ++counter);
         } while (await timer.WaitForNextTickAsync(token));
-    }
-
-    async Task StartReceiving(Socket party, CancellationToken token)
-    {
-        var streamInfo = RttStreamingInfo.InitWithPacketSize(_settings.PacketSize);
-
-        while (!token.IsCancellationRequested)
-        {
-            streamInfo = await ReadStreamData(party, token, streamInfo);
-            if (streamInfo.IsDisconnectedPrematurely)
-                throw new DisconnectedPrematurelyException();
-
-            var message = streamInfo.Message;
-            if (_statsMap.TryRemove(message.SequenceNumber, out var stats))
-            {
-                stats.RttValue = Stopwatch.GetElapsedTime(stats.SendTimeTrace, message.Timetrace);
-                InvokeReceivedStatsEvent(this, new RttStatisticsEventArgs(stats.SequenceNumber, stats));
-            }
-        }
-
-        token.ThrowIfCancellationRequested();
     }
 }
