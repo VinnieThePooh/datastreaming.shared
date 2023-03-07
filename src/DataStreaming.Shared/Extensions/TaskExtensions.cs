9c61183968be8c071dd4dd48b4903009396ed2d4
Lashkevich Ryan <lashkevichrain@gmail.com>
2023-03-07 22:56:18 +0700

    fix(): TaskExtension fix

--- src/DataStreaming.Shared/Extensions/TaskExtensions.cs
+++ src/DataStreaming.Shared/Extensions/TaskExtensions.cs
@@ -5,14 +5,30 @@ public static class TaskExtensions
     public static async Task WhenAll(this IEnumerable<Task> tasks)
     {
         var allTasks = Task.WhenAll(tasks);
-
         try
         {
             await allTasks;
         }
-        catch
+        catch (Exception)
         {
-            throw allTasks.Exception!;
+            if (allTasks.Exception is not null)
+                throw allTasks.Exception.Flatten();
+            throw;
+        }
+    }
+
+    public static async Task WhenAll(params Task[] tasks)
+    {
+        var allTasks = Task.WhenAll(tasks);
+        try
+        {
+            await allTasks;
+        }
+        catch(Exception)
+        {
+            if (allTasks.Exception is not null)
+                throw allTasks.Exception.Flatten();
+            throw;
         }
     }
 }
